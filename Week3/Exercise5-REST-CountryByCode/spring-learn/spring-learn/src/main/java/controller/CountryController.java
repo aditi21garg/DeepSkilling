388bd04caa8f0279bfb51dc4faed55b192cd5df9
Aditi <gargu5916@gmail.com>
2026-07-14 20:02:12 +0530

    Added exercise

---
+++ Week3/Exercise5-REST-CountryByCode/spring-learn/spring-learn/src/main/java/controller/CountryController.java
@@ -0,0 +1,32 @@
+package com.cognizant.spring_learn.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.spring_learn.model.Country;
+import com.cognizant.spring_learn.service.CountryService;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @RequestMapping("/country")
+    public Country getCountryIndia() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Country country = (Country) context.getBean("india");
+        return country;
+    }
+
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+
+}
